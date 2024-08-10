beb204ad3175d53e078036c03fdcec35d07cf199
Patel <patelb4@student.douglascollege.ca>
2024-08-09 17:25:27 -0700

    first commit

---
+++ Final_Exam_1280_S24_start/script.js
@@ -0,0 +1,32 @@
+// get the date from the input field
+function getDate() {
+   
+}
+
+
+function start() {
+    const dateInput = document.getElementById('birthday').value;
+    const countdownDate = new Date(dateInput).getTime();
+
+    const countdownInterval = setInterval(() => {
+        var now = new Date().getTime();
+        var timeDistance = countdownDate - now;
+
+        const days = Math.floor(timeDistance / (1000 * 60 * 60 * 24));
+        const hours = Math.floor((timeDistance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        const minutes = Math.floor((timeDistance % (1000 * 60 * 60)) / (1000 * 60));
+        const seconds = Math.floor((timeDistance % (1000 * 60)) / 1000);
+        const deciseconds = Math.floor((timeDistance % 1000) / 100);
+
+        document.getElementById('days').innerHTML = days;
+        document.getElementById('hours').innerHTML = hours;
+        document.getElementById('minutes').innerHTML = minutes;
+        document.getElementById('seconds').innerHTML = seconds;
+        document.getElementById('deciseconds').innerHTML = deciseconds;
+
+        if (distance < 0) {
+            clearInterval(countdownInterval);
+            document.getElementById('demo').innerHTML = "Date is Expired";
+        }
+    }, 100);
+}
